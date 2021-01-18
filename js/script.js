03f52345b5ab6598af9eee97aa6418aa5e6191c5
langg <coddan.leaves25@gmail.com>
2021-01-18 14:09:47 +0300

    really 1 zadaniye

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+let numberOfFilms;
+
+let personalMovieDB = {
+        count: +"",
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+personalMovieDB.count = numberOfFilms;
+personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его?", "");
+personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его?", "");
+
+console.log(personalMovieDB);
